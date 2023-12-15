f5adac64738604f42d933dcbfb83421d77a45568
Eugênio Marques <79340794+iGenio@users.noreply.github.com>
2023-12-15 19:22:38 -0300

    Add files via upload

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,8 @@
-const url = 'https://raw.githubusercontent.com/iGenio/js-developer-portfolio/main/data/profile.json'
-
-async function buscarArquivo (url) {
+async function buscarArquivo () {
+    const url = 'https://raw.githubusercontent.com/iGenio/js-developer-portfolio/main/data/profile.json'
     const file = await fetch(url)
     const profileData = await file.json()
     return profileData
 }
+
+
--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@ function updateProfileInfo(profileData) {
     name.innerText = profileData.name // Pega a tag com o id e muda o texto dentro dele
 
     const job = document.getElementById('profile.job')
-    job.innerText = profileData.job
+    job.innerHTML = profileData.job
 
     const location = document.getElementById('profile.location')
     location.innerText = profileData.location
@@ -23,7 +23,52 @@ function updateProfileInfo(profileData) {
     email.href = `tel:${profileData.email}`
 }
 
+
+//Preenche as ferramentas
+function updateHardSkills(profileData) {
+    const hardskills = document.getElementById('profile.skills.hardskills')
+    hardskills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('')
+}
+
+//Preenche as softskills
+function updateSoftSkills(profileData) {
+    const softskills = document.getElementById('profile.skills.softskills')
+    softskills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
+}
+
+//Preenche os idiomas
+function updateLanguage(profileData) {
+    const language = document.getElementById('profile.language')
+    language.innerHTML = profileData.idiomas.map(idioma => `<li>${idioma}</li>`).join('')
+}
+
+//Preenche o portfólio
+function updatePortfolio(profileData) {
+    const portfolio = document.getElementById('profile.portfolio')
+    portfolio.innerHTML = profileData.portfolio.map(portfolio => `<li><h3 class="title github">${portfolio.nome}</h3>
+        <a href="${portfolio.url}" target="_blank">${portfolio.url}</a>
+    </li>`).join('')
+}
+
+function updateExperience(profileData) {
+    const experience = document.getElementById('profile.experience')
+    experience.innerHTML = profileData.experiencias.map(experience => `                    <li>
+    <h3 class="title">${experience.nome}</h3>
+    <p class="period">${experience.periodo}</p>
+    <p>
+        ${experience.descricao}
+    </p>
+</li>`).join('')
+}
+
 (async () => {
     const profileData = await buscarArquivo()
+    console.log(profileData)
     updateProfileInfo(profileData)
+    updateHardSkills(profileData)
+    updateSoftSkills(profileData)
+    updateLanguage(profileData)
+    updatePortfolio(profileData)
+    updateExperience(profileData)
 })()
+
